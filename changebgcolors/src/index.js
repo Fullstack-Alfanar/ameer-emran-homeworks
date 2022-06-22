f8a25755322dd489bb4d35d4a0b67c1b731c62bd
ameer <ameer.em.89@gmail.com>
2022-06-22 20:09:51 +0300

    add the first react homework

---
+++ changebgcolors/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Bgcolors from './component/Bgcolor.app';
+import './component/Bgcolor.app.css';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Bgcolors />
+  </React.StrictMode>
+);
+
+
